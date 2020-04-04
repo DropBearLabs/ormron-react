631892ab16b1c009ae00b03ceba3011e93cbc188
Inga Pflaumer <inga.pflaumer@gmail.com>
2020-04-04 17:41:10 +1100

    Alterations, env and effects on the damage

--- prototype/fight.js
+++ prototype/fight.js
@@ -15,14 +15,21 @@ let envs = [
     'air'
 ]
 
-function calculateAttack(physical, magical, effectsAttack, effectsDefend, env, elementAttack, elementDefend){
+let effects = [
+    'Numb',
+    'Blinded',
+]
+
+function calculateAttack(physical, magical, alterAttack, alterDefend, env, elementAttack, elementDefend, effectAttack, effectDefend){
     let elementSequence = ['air','fire','metal','earth','water'];
 
     let tiers = [ 
         -40, -30, -20, -10, 0, 10, 20, 30, 40
     ];
     let aTier = 4;
+    let bTier = 4;
 
+    // Check the environment
     if(env==='fog'){
         aTier = aTier-1;
     }
@@ -32,18 +39,8 @@ function calculateAttack(physical, magical, effectsAttack, effectsDefend, env, e
     if(env==='air'){
         aTier = aTier+1;
     }
-    const sequence = elementSequence.indexOf(elementAttack)-elementSequence.indexOf(elementDefend);
-    console.log("Here's the sequence", sequence)
-    if(sequence===1 || sequence===4){
-        console.log(elementAttack+" follows "+elementDefend+" attack lessens");
-        aTier = aTier-1;
-    }
-    if(sequence===-1){
-        console.log(elementAttack+" preceids "+elementDefend+" attack grows");
-        aTier = aTier+1;
-    }
 
-    effectsAttack.forEach(element => {
+    alterAttack.forEach(element => {
         if(element === 'Frightened'){
             aTier = aTier-1;
         }
@@ -58,7 +55,7 @@ function calculateAttack(physical, magical, effectsAttack, effectsDefend, env, e
         }
     });
 
-    effectsDefend.forEach(element => {
+    alterDefend.forEach(element => {
         if(element==="Defended"){
             aTier = aTier-1;
         }
@@ -79,13 +76,32 @@ function calculateAttack(physical, magical, effectsAttack, effectsDefend, env, e
     // Set to min and max
     aTier = aTier>8 ? 8 : aTier;
     aTier = aTier<0 ? 0 : aTier;
+    bTier = aTier
+    // Check the element sequence
+    const sequence = elementSequence.indexOf(elementAttack)-elementSequence.indexOf(elementDefend);
+    if(sequence===1 || sequence===4){
+        bTier = aTier-1;
+    }
+    if(sequence===-1){
+        bTier = aTier+1;
+    }
+
+    // Check the effects
+    if(effectAttack==='Numb'){
+        magical = null;
+    }
+
+    if(effectAttack==='Blinded'){
+        physical = null;
+    }
 
 
     const aPercent = 100 + tiers[aTier];
+    const bPercent = 100 + tiers[bTier];
     console.log("The attack was with ", physical, magical);
-    console.log("The resulting modificator is ", aPercent);
+    console.log("The resulting modificator is ", aPercent, bPercent);
     const physicalAttack = physical * aPercent / 100;
-    const magicalAttack = magical * aPercent / 100;
+    const magicalAttack = magical * bPercent / 100;
     console.log("Your physical attack is " + physicalAttack + ", Your magical attack is "+ magicalAttack + ", damage is "+ (physicalAttack + magicalAttack));
     return  [physicalAttack, magicalAttack, physicalAttack + magicalAttack]
 
--- prototype/fight.test.js
+++ prototype/fight.test.js
@@ -1,6 +1,6 @@
 const calculateAttack = require('./fight');
 
-test("One effect standouts", () => {
+test("One alteration", () => {
   expect(calculateAttack(10, 10, ["Frightened"], ["Defended"])).toEqual([8, 8, 16]);
   expect(calculateAttack(8, 5, ["Frightened"], ["Defended"])).toEqual([6.4, 4, 10.4]);
   expect(calculateAttack(4, 3, ["Frightened"], ["Defended"])).toEqual([3.2, 2.4, 5.6]);
@@ -13,7 +13,7 @@ test("One effect standouts", () => {
 });
 
 
-test('Two effects standouts', () => {
+test('Two alterations', () => {
     expect(calculateAttack(10, 10, ["Panicing", "Blessed"], ["Panicing", "Blessed"])).toEqual([10, 10, 20]);
     expect(calculateAttack(10, 10, ["Blessed"], ["Panicing", "Blessed"])).toEqual([11, 11, 22]);
     expect(calculateAttack(10, 10, ['Frightened', "Panicing"], ["Defended", "Blessed"])).toEqual([6, 6, 12]);
@@ -22,7 +22,7 @@ test('Two effects standouts', () => {
     expect(calculateAttack(8, 5, ['Frightened', "Panicing"], ["Panicing"])).toEqual([7.2, 4.5, 11.7]);
 });
 
-test('Environment and effects', () => {
+test('Environment and alterations', () => {
     expect(calculateAttack(10, 10, ["Frightened"], ["Defended"], 'fog')).toEqual([7, 7, 14]);
     expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], 'fog')).toEqual([11, 11, 22]);
     expect(calculateAttack(4, 3, ["Frightened"], ["Defended"], 'fog')).toEqual([2.8, 2.1, 4.9]);
@@ -31,15 +31,22 @@ test('Environment and effects', () => {
     expect(calculateAttack(8, 5, ['Frightened', "Panicing"], ["Panicing"], 'air')).toEqual([8, 5, 13]);
 });
 
-test('Elements and effects', () => {
-    expect(calculateAttack(10, 10, ["Frightened"], ["Defended"], null, 'fire', 'metal')).toEqual([9, 9, 18]);
-    expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], null, 'metal', 'fire')).toEqual([11, 11, 22]);
+test('Elements, environment and alterations', () => {
+    expect(calculateAttack(10, 10, ["Frightened"], ["Defended"], null, 'fire', 'metal')).toEqual([8, 9, 17]);
+    expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], null, 'metal', 'fire')).toEqual([12, 11, 23]);
     expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], null, 'fire', 'water')).toEqual([12, 12, 24]);
-    expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], 'fog', 'metal', 'fire')).toEqual([10, 10, 20]);
-    expect(calculateAttack(10, 10, ["Panicing", "Blessed"], ["Panicing", "Blessed"], null, 'metal', 'earth')).toEqual([11, 11, 22]);
-    expect(calculateAttack(10, 10, ["Panicing", "Blessed"], ["Panicing", "Blessed"], 'air', 'metal', 'earth')).toEqual([12, 12, 24]);
+    expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], 'fog', 'metal', 'fire')).toEqual([11, 10, 21]);
+    expect(calculateAttack(10, 10, ["Panicing", "Blessed"], ["Panicing", "Blessed"], null, 'metal', 'earth')).toEqual([10, 11, 21]);
+    expect(calculateAttack(10, 10, ["Panicing", "Blessed"], ["Panicing", "Blessed"], null, 'warer', 'air')).toEqual([10, 11, 21]);
+    expect(calculateAttack(10, 10, ["Panicing", "Blessed"], ["Panicing", "Blessed"], 'air', 'metal', 'earth')).toEqual([11, 12, 23]);
     expect(calculateAttack(8, 5, ["Reinforced"], ["Panicing"], null, 'fire', 'water')).toEqual([9.6, 6, 15.6]);
-    expect(calculateAttack(8, 5, ["Reinforced"], ["Panicing"], 'fog', 'metal', 'fire')).toEqual([8, 5, 13]);
-    expect(calculateAttack(8, 5, ["Panicing", "Blessed"], ["Panicing", "Blessed"], null, 'metal', 'earth')).toEqual([8.8, 5.5, 14.3]);
-    expect(calculateAttack(8, 5, ["Panicing", "Blessed"], ["Panicing", "Blessed"], 'air', 'metal', 'earth')).toEqual([9.6, 6, 15.6]);
+    expect(calculateAttack(8, 5, ["Reinforced"], ["Panicing"], 'fog', 'metal', 'fire')).toEqual([8.8, 5, 13.8]);
+    expect(calculateAttack(8, 5, ["Panicing", "Blessed"], ["Panicing", "Blessed"], null, 'metal', 'earth')).toEqual([8, 5.5, 13.5]);
+    expect(calculateAttack(8, 5, ["Panicing", "Blessed"], ["Panicing", "Blessed"], 'air', 'metal', 'earth')).toEqual([8.8, 6, 14.8]);
 });
+
+test("Elements, environment, alterations and effects", () => {
+    expect(calculateAttack(10, 10, ["Frightened"], ["Defended"], null, 'fire', 'metal', 'Numb')).toEqual([8, 0, 8])
+    expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], null, 'metal', 'fire', 'Blinded')).toEqual([0, 11, 11]);
+
+})
