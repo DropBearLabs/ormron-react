b6752be3d8acd69d9e762de33360945cd65e8a19
Inga Pflaumer <inga.pflaumer@gmail.com>
2020-04-05 12:39:45 +1000

    Moves checks and tests

--- prototype/fight.js
+++ prototype/fight.js
@@ -20,6 +20,17 @@ let effects = [
     'Blinded',
 ]
 
+let field = [
+    [{x: 4, y:1, taken: false}, {x:4, y:2, taken: false}, {x:4, y:3, taken: false}, {x:4, y:4, taken: false}],
+    [{x: 3, y:1, taken: false}, {x:3, y:2, taken: false}, {x:3, y:3, taken: false}, {x:3, y:4, taken: false}],
+    [{x: 2, y:1, taken: true}, {x:2, y:2, taken: false}, {x:2, y:3, taken: true}, {x:2, y:4, taken: false}],
+    [{x: 1, y:1, taken: false}, {x:1, y:2, taken: true}, {x:1, y:3, taken: false}, {x:1, y:4, taken: false}],
+    [{x: -1, y:1, taken: false}, {x:-1, y:2, taken: false}, {x:-1, y:3, taken: false}, {x:-1, y:4, taken: false}],
+    [{x: -2, y:1, taken: false}, {x:-2, y:2, taken: false}, {x:-2, y:3, taken: false}, {x:-2, y:4, taken: false}],
+    [{x: -3, y:1, taken: false}, {x:-3, y:2, taken: false}, {x:-3, y:3, taken: false}, {x:-3, y:4, taken: false}],
+    [{x: -4, y:1, taken: false}, {x:-4, y:2, taken: false}, {x:-4, y:3, taken: false}, {x:-4, y:4, taken: false}],
+]
+
 function calculateAttack(physical, magical, alterAttack, alterDefend, env, elementAttack, elementDefend, effectAttack, effectDefend){
     let elementSequence = ['air','fire','metal','earth','water'];
 
@@ -98,15 +109,48 @@ function calculateAttack(physical, magical, alterAttack, alterDefend, env, eleme
 
     const aPercent = 100 + tiers[aTier];
     const bPercent = 100 + tiers[bTier];
-    console.log("The attack was with ", physical, magical);
-    console.log("The resulting modificator is ", aPercent, bPercent);
+    //console.log("The attack was with ", physical, magical);
+    //console.log("The resulting modificator is ", aPercent, bPercent);
     const physicalAttack = physical * aPercent / 100;
     const magicalAttack = magical * bPercent / 100;
-    console.log("Your physical attack is " + physicalAttack + ", Your magical attack is "+ magicalAttack + ", damage is "+ (physicalAttack + magicalAttack));
+    //console.log("Your physical attack is " + physicalAttack + ", Your magical attack is "+ magicalAttack + ", damage is "+ (physicalAttack + magicalAttack));
     return  [physicalAttack, magicalAttack, physicalAttack + magicalAttack]
 
 }
 
-module.exports = calculateAttack;
+function sameCell(from, to){
+    return from.x===to.x && from.y===to.y;
+}
+function endOfTheField(from, to){
+    return from.x>4 || to.x>4 || from.y>4 || to.y>4;
+}
+function moveAvailable(from, to){
+    const xDiff = Math.abs(from.x-to.x);
+    const yDiff = Math.abs(from.y-to.y);
+    return (xDiff+yDiff)===1;
+}
+function calculateMove(from, to){
+    if((from.x>0 && to.x<0) || (from.x<0 && to.x>0)){
+        return("You can't make this move, it's on opposite territory");
+    }
+    if(!from.taken){
+        return("You can't make this move, the cell is not taken");
+    }
+    if(to.taken){
+        return("You can't make this move, the cell you trying to move to is taken");
+    }
+    if(sameCell(from, to)){
+        return("You can't move to the same cell");
+    }
+    if(!moveAvailable(from, to)){
+        return("You can't jump over a cell");
+    }
+    if(endOfTheField(from, to)){
+        return("You can't jump over a field");
+    }
+    return("Move is possible");
+}
+
+module.exports = { calculateAttack, calculateMove};
 
 
--- prototype/fight.test.js
+++ prototype/fight.test.js
@@ -1,6 +1,6 @@
-const calculateAttack = require('./fight');
+const {calculateAttack, calculateMove} = require('./fight');
 
-test("One alteration", () => {
+test("Attack with one alteration", () => {
   expect(calculateAttack(10, 10, ["Frightened"], ["Defended"])).toEqual([8, 8, 16]);
   expect(calculateAttack(8, 5, ["Frightened"], ["Defended"])).toEqual([6.4, 4, 10.4]);
   expect(calculateAttack(4, 3, ["Frightened"], ["Defended"])).toEqual([3.2, 2.4, 5.6]);
@@ -13,7 +13,7 @@ test("One alteration", () => {
 });
 
 
-test('Two alterations', () => {
+test('Attack with two alterations', () => {
     expect(calculateAttack(10, 10, ["Panicing", "Blessed"], ["Panicing", "Blessed"])).toEqual([10, 10, 20]);
     expect(calculateAttack(10, 10, ["Blessed"], ["Panicing", "Blessed"])).toEqual([11, 11, 22]);
     expect(calculateAttack(10, 10, ['Frightened', "Panicing"], ["Defended", "Blessed"])).toEqual([6, 6, 12]);
@@ -22,7 +22,7 @@ test('Two alterations', () => {
     expect(calculateAttack(8, 5, ['Frightened', "Panicing"], ["Panicing"])).toEqual([7.2, 4.5, 11.7]);
 });
 
-test('Environment and alterations', () => {
+test('Attack with environment and alterations', () => {
     expect(calculateAttack(10, 10, ["Frightened"], ["Defended"], 'fog')).toEqual([7, 7, 14]);
     expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], 'fog')).toEqual([11, 11, 22]);
     expect(calculateAttack(4, 3, ["Frightened"], ["Defended"], 'fog')).toEqual([2.8, 2.1, 4.9]);
@@ -31,7 +31,7 @@ test('Environment and alterations', () => {
     expect(calculateAttack(8, 5, ['Frightened', "Panicing"], ["Panicing"], 'air')).toEqual([8, 5, 13]);
 });
 
-test('Elements, environment and alterations', () => {
+test('Attack with elements, environment and alterations', () => {
     expect(calculateAttack(10, 10, ["Frightened"], ["Defended"], null, 'fire', 'metal')).toEqual([8, 9, 17]);
     expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], null, 'metal', 'fire')).toEqual([12, 11, 23]);
     expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], null, 'fire', 'water')).toEqual([12, 12, 24]);
@@ -45,8 +45,23 @@ test('Elements, environment and alterations', () => {
     expect(calculateAttack(8, 5, ["Panicing", "Blessed"], ["Panicing", "Blessed"], 'air', 'metal', 'earth')).toEqual([8.8, 6, 14.8]);
 });
 
-test("Elements, environment, alterations and effects", () => {
-    expect(calculateAttack(10, 10, ["Frightened"], ["Defended"], null, 'fire', 'metal', 'Numb')).toEqual([8, 0, 8])
+test("Attack with elements, environment, alterations and effects", () => {
+    expect(calculateAttack(10, 10, ["Frightened"], ["Defended"], null, 'fire', 'metal', 'Numb')).toEqual([8, 0, 8]);
     expect(calculateAttack(10, 10, ["Reinforced"], ["Panicing"], null, 'metal', 'fire', 'Blinded')).toEqual([0, 11, 11]);
 
+})
+
+test("Test one cell move", () => {
+    expect(calculateMove({x:3, y:3, taken: true}, {x:2, y:3, taken: false}, "MOVE")).toEqual("Move is possible");
+    expect(calculateMove({x:3, y:3, taken: true}, {x:4, y:3, taken: false}, "MOVE")).toEqual("Move is possible");
+    expect(calculateMove({x:3, y:3, taken: true}, {x:3, y:2, taken: false}, "MOVE")).toEqual("Move is possible");
+    expect(calculateMove({x:3, y:3, taken: true}, {x:3, y:4, taken: false}, "MOVE")).toEqual("Move is possible");
+    expect(calculateMove({x:3, y:3, taken: true}, {x:1, y:2, taken: false}, "MOVE")).toEqual("You can't jump over a cell");
+    expect(calculateMove({x:3, y:3, taken: true}, {x:2, y:2, taken: false}, "MOVE")).toEqual("You can't jump over a cell");
+    expect(calculateMove({x:1, y:4, taken: true}, {x:1, y:5, taken: false}, "MOVE")).toEqual("You can't jump over a field");
+    expect(calculateMove({x:1, y:2, taken: true}, {x:1, y:2, taken: false}, "MOVE")).toEqual("You can't move to the same cell");
+    expect(calculateMove({x:-1, y:2, taken: true}, {x:1, y:2, taken: false}, "MOVE")).toEqual("You can't make this move, it's on opposite territory");
+    expect(calculateMove({x:1, y:2, taken: false}, {x:1, y:2, taken: false}, "MOVE")).toEqual("You can't make this move, the cell is not taken");
+    expect(calculateMove({x:1, y:2, taken: true}, {x:1, y:2, taken: true}, "MOVE")).toEqual("You can't make this move, the cell you trying to move to is taken");
+    expect(calculateMove({x:1, y:2, taken: true}, {x:2, y:2, taken: false}, "MOVE")).toEqual("Move is possible");
 })
